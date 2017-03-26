dd3bc76a1efc90e7bf4a63e7e007e7b740aeace5
jassmin586 <joannarz52@gmail.com>
2017-03-26 19:55:05 +0200

    Rename variables

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-$("ul").on("click", "button", function(e) {
-  var $this = $(this);
-  var $selected = $this.hasClass("purchased");
-  var $deleted = $this.hasClass("delete");
+$(function(){
 
-  if ($selected) {
-    $this.prev().addClass("checked");
-    $this.text("Delete")
-        .removeClass("purchased")
-        .addClass("delete");
+  $("ul").on("click", "button", function(e) {
+    var $this = $(this);
+    var selected = $this.hasClass("purchased");
+    var deleted = $this.hasClass("delete");
 
-    var $moveItem = $this.prev().detach();
-    var $moveBtn = $this.detach();
-    $moveItem.appendTo("#checked-products");
-    $moveBtn.appendTo("#checked-products");
+    if (selected) {
+      $this.prev().addClass("checked");
+      $this.text("Delete")
+          .removeClass("purchased")
+          .addClass("delete");
 
+      var $moveItem = $this.prev().detach();
+      var $moveBtn = $this.detach();
+      $moveItem.appendTo("#checked-products");
+      $moveBtn.appendTo("#checked-products");
+
+      updateAmount();
+    }
+    if (deleted) {
+      $this.prev().remove();
+      $this.remove();
+    }
+
+  });
+
+  $("#newItem").on("submit", function(e) {
+    var inputText = $("input:text").val();
+    var $productList = $("#current-products");
+    e.preventDefault();
+    $productList.append("<li class=\"product\">" + inputText + "</li>").hide()
+               .append("<button type=\"button\" name=\"purchased\" class=\"purchased\">Check</button>").hide()
+               .fadeIn(700);
+    $("input:text").val("");
     updateAmount();
-  }
-  if ($deleted) {
-    $this.prev().remove();
-    $this.remove();
+  });
+
+  function updateAmount() {
+    var amount = $("#current-products").children(".product").length;
+    $("#amount").text(amount);
   }
 
 });
-
-$("#newItem").on("submit", function(e) {
-  var inputText = $("input:text").val();
-  var productList = $("#current-products");
-  e.preventDefault();
-  productList.append("<li class=\"product\">" + inputText + "</li>").hide()
-             .append("<button type=\"button\" name=\"purchased\" class=\"purchased\">Check</button>").hide()
-             .fadeIn(700);
-  $("input:text").val("");
-  updateAmount();
-});
-
-function updateAmount() {
-  var amount = $("#current-products").children(".product").length;
-  $("#amount").text(amount);
-}
